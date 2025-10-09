2c65669ee8cb8f828e836fda897927cdcf44c992
Monarch <bryandhaniel123@gmail.com>
2025-10-09 21:02:08 +0700

    fix: fix foods route

--- src/app/api/foods/[id]/route.ts
+++ src/app/api/foods/[id]/route.ts
@@ -4,10 +4,10 @@ import { NextResponse, NextRequest } from "next/server";
 
 export async function GET(
   request: NextRequest,
-  { params }: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
   try {
-    const { id } = params;
+    const { id } = await params; // Await the params
 
     const client = await clientPromise;
     const db = client.db("elpis_db");
