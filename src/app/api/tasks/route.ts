40b0644b1c793b4e339b77dd2a0791f6eacfbcf5
Monarch <bryandhaniel123@gmail.com>
2025-10-09 20:27:40 +0700

    fix: add console log

--- src/app/api/tasks/route.ts
+++ src/app/api/tasks/route.ts
@@ -3,17 +3,18 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   try {
+    console.log("✅ Connected to MongoDB:", process.env.MONGODB_URI ? "Yes" : "No");
     const client = await clientPromise;
-    const db = client.db("elpis_db"); // Ganti "elpis" dengan nama database Anda
-
+    const db = client.db("elpis_db");
     const tasks = await db.collection("tasks").find({}).toArray();
 
     return NextResponse.json({ tasks });
   } catch (e) {
-    console.error(e);
+    console.error("❌ Error fetching all tasks:", e);
     return NextResponse.json({ error: "Unable to fetch tasks" }, { status: 500 });
   }
 }
+
 
 export async function POST(request: Request) {
   try {
