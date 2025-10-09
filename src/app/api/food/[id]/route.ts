0dc60347ed73378606183de48e41ecce7b9a3590
Monarch <bryandhaniel123@gmail.com>
2025-10-09 19:48:37 +0700

    fix: fix how context params typed

--- src/app/api/food/[id]/route.ts
+++ src/app/api/food/[id]/route.ts
@@ -1,19 +1,17 @@
 import clientPromise from "@/lib/mongodb";
 import { ObjectId } from "mongodb";
-import { NextResponse } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 
 export async function GET(
-  request: Request,
-  { params }: { params: { id: string } }
+  request: NextRequest,
+  context: { params: Promise<{ id: string }> }
 ) {
   try {
-    const client = await clientPromise;
-    const db = client.db("elpis_db"); // Ganti "elpis" dengan nama database Anda
-    const id = params.id;
+    const { id } = await context.params; 
 
-    const food = await db
-      .collection("foods")
-      .findOne({ _id: new ObjectId(id) });
+    const client = await clientPromise;
+    const db = client.db("elpis_db");
+    const food = await db.collection("foods").findOne({ _id: new ObjectId(id) });
 
     if (!food) {
       return NextResponse.json({ error: "Food item not found" }, { status: 404 });
--- src/app/api/tasks/[id]/route.ts
+++ src/app/api/tasks/[id]/route.ts
@@ -1,19 +1,18 @@
 import clientPromise from "@/lib/mongodb";
 import { ObjectId } from "mongodb";
-import { NextResponse } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 
 export async function GET(
-  request: Request,
-  { params }: { params: { id: string } }
+  request: NextRequest,
+  context: { params: Promise<{ id: string }> }
 ) {
   try {
-    const client = await clientPromise;
-    const db = client.db("elpis_db"); // Ganti "elpis" dengan nama database Anda
-    const id = params.id;
+    const { id } = await context.params; 
 
-    const task = await db
-      .collection("tasks")
-      .findOne({ _id: new ObjectId(id) });
+    const client = await clientPromise;
+    const db = client.db("elpis_db");
+
+    const task = await db.collection("tasks").findOne({ _id: new ObjectId(id) });
 
     if (!task) {
       return NextResponse.json({ error: "Task not found" }, { status: 404 });
